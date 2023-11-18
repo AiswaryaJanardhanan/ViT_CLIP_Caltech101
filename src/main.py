98e028d0f4c904e14758edf56f1c08f69785754a
Aiswarya Janardhanan <aiswaryasreelatha98@gmail.com>
2023-11-18 09:09:45 -0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import shutil
+from torchvision import datasets, transforms
+src_dir = './'
+os.chdir(src_dir)
+import dataload
+train_loader, test_loader  = dataload.load_dataset( 'Caltech101', 1000, max_num = None)
+import train 
+model = train.load_model(False,10,102, 'vit_b_16')
+print(model)
