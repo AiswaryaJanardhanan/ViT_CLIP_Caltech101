8c4a75109c32074732e02dc3102d02eadaf14e5b
Aiswarya Janardhanan <aiswaryasreelatha98@gmail.com>
2023-11-19 07:40:21 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,39 +8,52 @@ import shutil
 from torchvision import datasets, transforms
 import yaml
 from utils import mkdir_p
-
-
+import argparse
 config_file = '../env.yml'
 with open(config_file, 'r') as stream:
     yamlfile = yaml.safe_load(stream)
     root_dir = yamlfile['root_dir']
     src_dir = yamlfile['src_dir']
-# root_dir = '/content/drive/MyDrive/ECE_562_Project/root'
-# src_dir = '/content/drive/MyDrive/ECE_562_Project/src'
-pretrained = True
-epochs=500
 
-if pretrained ==True:
-    layerID = 0
-else:
-    layerID =7
+if __name__ == '__main__':
 
-os.chdir(src_dir)
-import dataload
-train_loader, test_loader  = dataload.load_dataset( 'Caltech101', 6902, max_num = None)
-import train
-model = train.load_model(pretrained,layerID,101, 'vit_b_16')
-model = train.Train_Model(model, train_loader,test_loader,pretrained,epochs)
+    parser = argparse.ArgumentParser(description='PyTorch Decision-based Membership Inference Attack Toy Example') 
 
-cluster = len(train_loader.dataset)
-delta = {}
-# model.load_state_dict(torch.load(root_dir + '/model/'+'_Epoch_'+str(args.epochs)))
-# model.cuda()
-model.eval()
-acc_test= train.test(model, test_loader, verbos=True)
-acc_train= train.test(model, train_loader, verbos=True)
-                    
-delta['acc_test'] = acc_test
-delta['acc_train'] = acc_train
+    parser.add_argument('--dataset_name', default='Caltech101', type=str, help='cifar10, cifar100, gtsrb')
+    parser.add_argument('--epochs', type=int, default=500, metavar='N',
+                        help='number of epochs to train (default: 200)')
+    parser.add_argument('--numClasses', type=int, default=101, metavar='N',
+                        help='number of classes(default: 101 in Caltech101)')
+
+    parser.add_argument('--modelName',type=str, default='vit_b_16', help = 'vit_b_16,ResNet18,Vgg-16')
+    parser.add_argument('--pretrained', type=str, default=True, help = 'True/ False')
+    args = parser.parse_args()
+
+
+    pretrained = args.pretarined
+    epochs=args.epochs
+
+    if pretrained ==True:
+        layerID = 0
+    else:
+        layerID =7
+
+    os.chdir(src_dir)
+    import dataload
+    train_loader, test_loader  = dataload.load_dataset( args.dataset_name, 6902, max_num = None)
+    import train
+    model = train.load_model(pretrained,layerID,args.numClasses, args.modelName)
+    model = train.Train_Model(model, train_loader,test_loader,pretrained,epochs)
+
+    cluster = len(train_loader.dataset)
+    delta = {}
+    # model.load_state_dict(torch.load(root_dir + '/model/'+'_Epoch_'+str(args.epochs)))
+    # model.cuda()
+    model.eval()
+    acc_test= train.test(model, test_loader, verbos=True)
+    acc_train= train.test(model, train_loader, verbos=True)
                         
-np.save(root_dir + '/results_0.0001'+'_'+'_'+str(layerID)+'_'+str(cluster)+'_'+str(epochs)+'_.npy', delta)
+    delta['acc_test'] = acc_test
+    delta['acc_train'] = acc_train
+                            
+    np.save(root_dir + '/results_0.0001'+'_'+'_'+str(layerID)+'_'+str(cluster)+'_'+str(epochs)+'_.npy', delta)
