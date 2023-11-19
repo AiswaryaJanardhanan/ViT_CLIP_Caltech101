23d5dc65fab0ea96cb8d893c51080a21a05e4085
Aiswarya Janardhanan <aiswaryasreelatha98@gmail.com>
2023-11-19 07:49:34 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    pretrained = args.pretarined
+    pretrained = args.pretrained
     epochs=args.epochs
 
     if pretrained ==True:
