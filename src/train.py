d3c6f66200657592f5a829dbc32c9025487f3af9
Aiswarya Janardhanan <62233017+AiswaryaJanardhanan@users.noreply.github.com>
2023-12-07 08:27:19 -0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import torch
 import torch.optim as optim
 import torch
 import torch.optim as optim
-from torchvision.models import vit_b_16
+from torchvision.models import vit_b_16,vit_b_32
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
@@ -14,20 +14,33 @@ with open(config_file, 'r') as stream:
     yamlfile = yaml.safe_load(stream)
     root_dir = yamlfile['root_dir']
     src_dir = yamlfile['src_dir']
-# root_dir = '/content/drive/MyDrive/ECE_562_Project/root'
-# src_dir = '/content/drive/MyDrive/ECE_562_Project/src'
 
 
-# Define the ViT-B-16 model
+# Define the loading model function ViT-B-16 model
 def load_model(pretrained,layerid,NUM_CLASSES, model_name):
 	if(model_name == 'vit_b_16'):
-		if pretrained:
+
+		if pretrained == True:
+			print('from pretrained')
 			model = vit_b_16(weights='DEFAULT')
 		else:
-			model = vit_b_16(weights=None)
+			print('from scratch')
+			model = vit_b_16(pretrained =False)
+		
 		model=layerFreezing(model,layerid,NUM_CLASSES, model_name, pretrained)
+		
+		return model
+	
+	if(model_name == 'vit_b_32'):
+		if pretrained == True:
+			print('from pretrained')
+			model = vit_b_32(weights='DEFAULT')
+		else:
+			print('from scratch')
+			model = vit_b_32(pretrained =False,num_classes= NUM_CLASSES)
 		return model
 
+# layer freezing function for transfer learning replacing last layer model head to have same output as number of classes 
 def layerFreezing(model,layerid,NUM_CLASSES, model_name='vit_b_16', pretrained=False):
 
 	
@@ -37,7 +50,7 @@ def layerFreezing(model,layerid,NUM_CLASSES, model_name='vit_b_16', pretrained=F
 	weights of the Pretrained model are used to train the Target model.
 	"""
 	
-	if pretrained == False: 
+	if pretrained == 'False': 
 		layerid=0
 		
 	count=0
@@ -82,80 +95,16 @@ def layerFreezing(model,layerid,NUM_CLASSES, model_name='vit_b_16', pretrained=F
 							  nn.ReLU(),
 							  nn.Dropout(0.5),
 							  nn.Linear(512, NUM_CLASSES),)		
+	elif model_name == 'vit_b_32':
+		model.head = nn.Sequential(
+							  nn.Linear(768, 512),
+							  nn.ReLU(),
+							  nn.Dropout(0.5),
+							  nn.Linear(512, NUM_CLASSES),)		
+									
 	return model
 
-# #Test Function
-# def test(model, test_loader, verbos=True):
-
-# 	test_loss = 0
-# 	correct = 0
-# 	model.eval()
-# 	with torch.no_grad():
-# 		for data, target in test_loader:
-# 			data, target = data.cuda(), target.cuda()
-# 			output = model(data)
-# 			test_loss += F.cross_entropy(output, target).item()
-# 			pred = output.max(1, keepdim=True)[1]
-# 			correct += pred.eq(target.view_as(pred)).sum().item()
-
-# 	test_loss /= len(test_loader.dataset)
-
-# 	accuracy = 100. * correct / len(test_loader.dataset)
-# 	if verbos:
-# 		print('\n Test Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-# 			test_loss, correct, len(test_loader.dataset), accuracy))
-# 	return accuracy
-
-# ##Train function
-# def train(model, train_loader, optimizer, epoch):
-#   cnt = 0
-#   for batch_idx, (data, target) in enumerate(train_loader):
-#       data, target = data.cuda(), target.cuda()
-#       # print(batch_idx, " - ",f"Data shape: {data.shape}",f"Target shape: {target.shape}")
-#       optimizer.zero_grad()
-#       output = model(data)
-#       loss = F.cross_entropy(output, target)
-#       loss.backward()
-#       optimizer.step()
-#       cnt +=1
-#   print('size of training data covered', cnt)
-#   acc= test(model,train_loader,verbos=False)
-#   print('TargetModel Train Epoch: {} \tLoss: {:.6f}, \t Accuracy: {:.2f}'.format(epoch,loss.item(),acc))
-
-#   return acc
-
-##training model and saving checkpoint 
-# def Train_Model(model, train_loader,test_loader,pretrained, epochs):
-# 	layerID = 0
-# 	torch.cuda.empty_cache()
-# 	cluster = len(train_loader.dataset)
-# 	description= ''
-# 	if pretrained ==False:
-# 		description+='vitb16_trSize_' + str(cluster)
-# 	else:
-# 		layerID =7
-# 		description+='vitb16_LayerID_'+str(layerID)
-# 	save_path= root_dir +'/model/'+	description
-# 	if not os.path.exists(save_path+'_Epoch_'+str(epochs)):
-# 		print('***'*10,'Saving model to: ', save_path+'_Epoch_'+str(epochs))
-# 		model.train()
-# 		model.cuda()
-# 		optimizer = optim.Adam(model.parameters(), lr=0.0001)
-# 		# optimizer = torch.optim.SGD(model.parameters(), lr=0.01,momentum=0.9, weight_decay=5e-4)
-# 		for epoch in range(1, epochs + 1):
-# 			train( model, train_loader, optimizer, epoch)
-# 			if epoch%100==0:
-# 				torch.save(model.state_dict(), save_path+'_Epoch_'+str(epoch))
-# 		torch.save(model.state_dict(), save_path+'_Epoch_'+str(epochs))
-# 	else:
-# 		print(save_path+'_Epoch_'+str(epochs))
-# 		print('***'*10,'Loading model from: ', save_path+'_Epoch_'+str(epochs))
-# 		model.load_state_dict(torch.load(save_path+'_Epoch_'+str(epochs)))
-# 		model.cuda()		
-# 	test(model, test_loader)
-
-# 	return model
-
+# traing model function
 def train_model(model, criterion, optimizer, scheduler,dataloaders,dataset_sizes, num_epochs=25):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -233,6 +182,7 @@ def train_model(model, criterion, optimizer, scheduler,dataloaders,dataset_sizes
 
     return model
 
+# function to test model
 def test_model(model_ft, dataloaders_test, Num_class, mode = 'test'):
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
