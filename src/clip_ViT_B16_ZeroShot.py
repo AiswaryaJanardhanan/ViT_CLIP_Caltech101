6bd5c80bbe3dc863a256b2a2d9fea853355a8e2e
Aiswarya Janardhanan <62233017+AiswaryaJanardhanan@users.noreply.github.com>
2023-12-06 22:22:01 -0800

    Update clip_ViT_B16_ZeroShot.py

--- src/clip_ViT_B16_ZeroShot.py
+++ src/clip_ViT_B16_ZeroShot.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 import torch
 import os
-from torchvision.models import vit_b_16
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 import torch.optim as optim
@@ -11,7 +10,8 @@ import torch.nn.functional as F
 import yaml
 import argparse
 import shutil
-
+import torch.nn as nn
+import torch.nn.functional as F
 import clip
 
 config_file = '../env.yml'
@@ -19,32 +19,30 @@ with open(config_file, 'r') as stream:
     yamlfile = yaml.safe_load(stream)
     root_dir = yamlfile['root_dir']
     src_dir = yamlfile['src_dir']
-  
-classes = os.listdir(root_dir + '/data/train/')
+
+classes = os.listdir(root_dir + '/data/test/')
 
 # Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load('ViT-B/16', device)
+model, preprocess = clip.load("ViT-B/16", device)
 transform = preprocess
 
 data = {
-    'train': datasets.ImageFolder(root=root_dir +'train'),
-    'eval': datasets.ImageFolder(root=root_dir+'eval'),
-    'test': datasets.ImageFolder(root=root_dir+'test')
+    'train': datasets.ImageFolder(root=root_dir +'/data/train'),
+    'eval': datasets.ImageFolder(root=root_dir+'/data/eval'),
+    'test': datasets.ImageFolder(root=root_dir+'/data/test')
 }
 test_set = data['test']
+cnt = 0
+total_cnt = len(test_set)
+for image_input, class_id in test_set:
+  # print(class_id)
+  # print(classes[class_id])  # # from PIL import Image
 
-correct_predictions = 0
-total_predictions = 0
-
-
-for i in range(len(test_set)):
-
-  image_input, class_id = test_set[10]
-
+  # # # # Preprocess the image
   image_input = preprocess(image_input).unsqueeze(0).to(device)
-
   text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in classes]).to(device)
+
   # # # Calculate features
   with torch.no_grad():
       image_features = model.encode_image(image_input)
@@ -54,16 +52,15 @@ for i in range(len(test_set)):
   image_features /= image_features.norm(dim=-1, keepdim=True)
   text_features /= text_features.norm(dim=-1, keepdim=True)
   similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-  values, indices = similarity[0].top(5)
-    # Get the predicted class
-  predicted_class = similarity[0].argmax().item()
+  print(similarity)
+  values, indices = similarity[0].topk(1)
 
-    # Check if the prediction is correct
-  if predicted_class == class_id:
-      correct_predictions += 1
-
-  total_predictions += 1
-
-# Calculate accuracy
-accuracy = (100.0 * correct_predictions) / total_predictions
-print(f"\nAccuracy: {accuracy:.2f}%")
+  # Print the result
+  # print("\nTop predictions:\n")
+  for value, index in zip(values, indices):
+      # print(f"{classes[index]:>16s}: {100 * value.item():.2f}%")
+      if(index == class_id):
+        cnt= cnt+1
+        print('image_input',image_input)
+        print('class_id',class_id)
+print("Accuracy of clip_B_16 : ",cnt/total_cnt)
